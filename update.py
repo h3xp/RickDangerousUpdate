5f5ac982a924eee100ef0cb22f7555cc3337b3a7
h3xp <tomniklas94@gmail.com>
2022-08-19 08:06:50 +0200

    EOF exception

--- update.py
+++ update.py
@@ -167,7 +167,7 @@ def main_menu():
         try:
             uinp = input('\n Enter your Selection: ')
         except EOFError:
-            print("EOF")
+            main_menu()
         if uinp == '1':
             improvements_menu()
             break
