72ae235a4b0291354c39c0a5d626167cafcab8eb
h3xp <tomniklas94@gmail.com>
2022-08-19 07:44:26 +0200

    run.sh for easy installation and usage

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ This script makes the installation process very easy.
 It installs pip3 (necessary to install package dependencies), clones the repository of the updater,
 installs dependencies and starts the updater.
 '
-sudo apt-get install python3-pip -y && git clone https://github.com/h3xp/RickDangerousUpdate && cd RickDangerousUpdate && pip3 install -r requirements.txt && python3 update.py
+sudo apt-get install python3-pip -y &&
+git clone https://github.com/h3xp/RickDangerousUpdate &&
+cd RickDangerousUpdate &&
+pip3 install -r requirements.txt &&
+python3 update.py
