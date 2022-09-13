efc4e5ab338c64ea232eb56791200095ab988ccb
LizardWiz <lizardwiz@hotmail.com>
2022-09-12 20:01:04 -0700

    Debugging

--- install.py
+++ install.py
@@ -223,7 +223,7 @@ def install(overwrite=True):
         with open(new_gamelist_path, "wb") as fh:
             src_tree.write(fh, "utf-8")
 
-        with open(new_gamelist_path + "-new", "wb") as fh:
+        with open(gamelist_file + "-new", "wb") as fh:
             src_tree.write(fh, "utf-8")
 
         if os.path.getsize(new_gamelist_path) > 0:
