e8c9e64971efe66c1d1bdff4428754bd66a7bb6d
h3xp <tomniklas94@gmail.com>
2022-10-06 03:10:56 +0200

    fixing install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,4 @@ installs dependencies and starts the updater.
 
 parameters [git repo, mega link (optional), "-update" or "-remove" (optional)]
 '
-if [ -z "$2" ]
-then
-  git_branch="main"
-else
-  git_branch=$2
-fi
-sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/$git_branch/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/$git_branch/install.py" -s -N) $git_branch $3
+sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/install.py" -s -N) "main" $3
