bf14207fd874575ed39e386a55b7c0eb30d32bab
mrludo001 <109332089+mrludo001@users.noreply.github.com>
2023-03-03 20:48:42 +1100

    Update install.sh
    Undo this before merging to dev

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ This script makes running the script very easy.
 It installs pip3 (necessary to install package dependencies),
 installs dependencies and starts the updater.
 '
-sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/install.py" -s -N) $1 $2
+sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/oss/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/install.py" -s -N) $1 $2
