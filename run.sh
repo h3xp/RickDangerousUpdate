08e8d3464777f9bb678cde6c0f47059ab7390ab9
mrludo001 <109332089+mrludo001@users.noreply.github.com>
2023-03-07 18:13:57 +1100

    Update run.sh
    Switched oss back to dev.

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 : '
 This script makes running the script very easy.
-It installs pip3 (necessary to install package dependencies),
+It refreshes apt repository info,
+installs pip3 (necessary to install package dependencies),
 installs dependencies and starts the updater.
 '
 if [ "$EUID" -eq 0 ]
   then echo "Do not run this script as root. Do not use sudo."
   exit
 fi
-sudo apt-get update && sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/oss/update.py" -s -N) $1
+sudo apt-get update && sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/dev/update.py" -s -N) $1
