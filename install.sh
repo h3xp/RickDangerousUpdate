415b808d869e0221bda079a88119affcf84383cf
mrludo001 <109332089+mrludo001@users.noreply.github.com>
2023-03-11 11:10:22 +1100

    Update install.sh
    Point back to main for merge and release.

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ It refreshes apt repository information,
 installs pip3 (necessary to install package dependencies),
 installs dependencies and starts the updater.
 '
-sudo apt-get update && sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/dev/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/dev/install.py" -s -N) $1 $2
+sudo apt-get update && sudo apt-get -y install python3-pip && pip3 install -r <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/dev/requirements.txt" -s -N) && python3 <(curl "https://raw.githubusercontent.com/h3xp/RickDangerousUpdate/main/install.py" -s -N) $1 $2
