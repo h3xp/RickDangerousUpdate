a01153fc026717b2b2cd7889619a4e2416b8759f
mrludo001 <109332089+mrludo001@users.noreply.github.com>
2023-05-11 22:55:54 +1000

    Update update.py
    Fixed stupid flub.

--- update.py
+++ update.py
@@ -238,7 +238,7 @@ def get_manual_updates_story():
 
     megadrive = check_drive()
 
-    update_dir = get_valid_path_portion(())
+    update_dir = get_valid_path_portion(get_default_update_dir())
     update_dir = manual_updates_dialog(update_dir, False)
     update_dir = get_config_value("CONFIG_ITEMS", "update_dir")
     
